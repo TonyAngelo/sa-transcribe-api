0630f85b8f3c3e41d46578cab1a38f63f576a17f
Anthony Petrangelo <tonypetrangelo@gmail.com>
2019-03-06 05:24:12 -0600

    fixed get vocab

--- vocabGet.js
+++ vocabGet.js
@@ -1,17 +1,27 @@
 import { success, failure } from "./libs/response-lib";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+//import AWS from "aws-sdk";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
-  var transcribeservice = new AWS.TranscribeService();
-  var params = {
-    VocabularyName: data['name']
+  //var transcribeservice = new AWS.TranscribeService();
+  // var params = {
+  //   VocabularyName: data['name']
+  // };
+  const params = {
+    TableName: 'audio-vocabulary',
+    Key: {
+      vocabName: data['name']
+    }
   };
   try {
-    var result = await transcribeservice.getVocabulary(params).promise();
-    //console.log(result);
-    
-    return success(result);
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      // Return the retrieved item
+      return success(result.Item);
+    } else {
+      return failure({ status: false, error: "Item not found." });
+    }
   } catch (e) {
     return failure({ status: false });
   }
