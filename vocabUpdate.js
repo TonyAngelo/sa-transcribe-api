0209a75413741247f3cd4d0c684e6c33910a46a4
Anthony Petrangelo <tonypetrangelo@gmail.com>
2019-03-06 08:44:18 -0600

    updated the vocab update again

--- vocabUpdate.js
+++ vocabUpdate.js
@@ -3,14 +3,20 @@ import AWS from "aws-sdk";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
-  var stepfunctions = new AWS.StepFunctions();
+  //var stepfunctions = new AWS.StepFunctions();
+  // var transcribeservice = new AWS.TranscribeService();
+  // var params = {
+  //   stateMachineArn: 'arn:aws:states:us-east-1:741625714969:stateMachine:audio-transcribe-createVocabulary',
+  //   input: "{\"list\" : \"" + data['list'] + "\"}",
+  //   name: Math.round((new Date()).getTime() / 1000).toString()
+  // };
   var params = {
-    stateMachineArn: 'arn:aws:states:us-east-1:741625714969:stateMachine:audio-transcribe-createVocabulary',
-    input: "{\"list\" : \"" + data['list'] + "\"}",
-    name: Math.round((new Date()).getTime() / 1000).toString()
+    LanguageCode: 'en-US',
+    Phrases: data['list'],
+    VocabularyName: data['name']
   };
   try {
-    var result = await stepfunctions.startExecution(params).promise();
+    var result = await transcribeservice.updateVocabulary(params).promise();
     //console.log(result);
     
     return success(result);
