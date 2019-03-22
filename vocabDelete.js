82df5229ed6dbdcb3c393844faecc8784936801f
Anthony Petrangelo <tonypetrangelo@gmail.com>
2019-03-22 11:02:12 -0500

    updated vocab delete method

--- vocabDelete.js
+++ vocabDelete.js
@@ -1,15 +1,26 @@
 import { success, failure } from "./libs/response-lib";
 import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
-  var transcribeservice = new AWS.TranscribeService();
-  var params = {
-    VocabularyName: data['name']
+  //var transcribeservice = new AWS.TranscribeService();
+  // var params = {
+  //   VocabularyName: data['name']
+  // };
+  const params = {
+    TableName: 'audio-vocabulary',
+    Key:{
+      vocabName: data['name'],
+    }
   };
   try {
-    var result = await transcribeservice.deleteVocabulary(params).promise();
+    //var result = await transcribeservice.deleteVocabulary(params).promise();
     //console.log(result);
+    var result = await dynamoDbLib.call("delete", params);
+    console.log('success');
+    console.log(result);
+    //return success(params.Item);
     
     return success(result);
   } catch (e) {
